95813b1367d98a82b5b1b7fa83ecaa131bd9ae96
timetravel-1010 <timetravel_1010@protonmail.com>
2022-11-29 14:38:37 -0500

    Agregar cambios

--- Frontend/src/Result.js
+++ Frontend/src/Result.js
@@ -2,15 +2,22 @@ import { useEffect, useRef } from "react";
 
 export default function Result({response}) {
     const resultadoRef = useRef(null);
-
-    console.log(response);
+    
+    
 
     useEffect(() => {
         resultadoRef.current.style.visibility = "hidden"
     }, []);
 
-    if(Object.keys(response).length != 0)
+    if(Object.keys(response).length != 0) {
         resultadoRef.current.style.visibility = "visible"
+        console.log(typeof(response))
+        console.log(response)
+        //response = JSON.stringify(response)
+        //const obj = JSON.parse(response)
+        //console.log(obj)
+        //console.log("temas: ", obj.temas);
+    }
 
     return (
         <div ref={resultadoRef}>
